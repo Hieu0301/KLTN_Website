9bc0b42bdbcd1b89ea95b32324a277ec4bc85c73
Hieu0301 <phamminhhieu.03012002@gmail.com>
2024-12-05 16:47:26 +0700

    deploy 6

--- server/index.js
+++ server/index.js
@@ -50,6 +50,20 @@ app.use(
   })
 );
 
+
+//------- Deploy
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "../client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
+
 // Import Socket Initialization
 const { initSocket } = require("./socket");
 
